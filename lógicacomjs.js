e2f551d6e9950413bb74b61a4a25686bef233215
sandrielu1005 <sansilva7712@gmail.com>
2025-05-22 17:14:21 -0300

    aprendendo prompt

---
+++ lógicacomjs.js
@@ -0,0 +1,6 @@
+let x = Number (prompt("digite um numero"))
+console.log(x) 
+let salario = Number (prompt("digite seu salario"))
+let doze =
+let mesestrabalhados =12
+console.log(x) ("seus décimo é =" + variavel) 
