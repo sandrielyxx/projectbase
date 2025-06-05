4ebd0ed1a43e7ffa229cb1b2442e04c0853c62e0
sandrielu1005 <sansilva7712@gmail.com>
2025-06-05 16:58:51 -0300

    aprendendo array

--- lógicacomjs.js
+++ lógicacomjs.js
@@ -1,6 +1,82 @@
-let x = Number (prompt("digite um numero"))
+/*let x = Number (prompt("digite um numero"))
 console.log(x) 
 let salario = Number (prompt("digite seu salario"))
 let doze =
 let mesestrabalhados =12
-console.log(x) ("seus décimo é =" + variavel) 
+console.log(x) ("seus décimo é =" + variavel) */
+ 
+/*let salario = number (prompt("digite seus salario"))
+if (salario < 500) {
+    salario = (15/100*)+salario 
+    alert ("seus novo salario é:"+ salario)
+}else if ("salario >=500 && salario<1000"){
+    salario = (10/100*salario)+salario
+    alert ("seus novo salario é: "+salario)
+}else{
+    salario = ("5/100*salario é:"+salario )
+}*/
+
+/*let idade Number(prompt("digite sua idade"))
+if (idade <12){
+    akert ("recomendavel conteudo infantil")
+}
+else if (idade =<12 && idade  <=17){
+    alert ("recomendavel conteudo teen")
+}
+else if (idade >=18 && idade <=60){
+    alert ("recomendavel conteudo adulto")
+}
+else{
+    alert ("recomendavel conteudo senior")
+}
+
+
+
+let valordobasico =25;
+let valorpremium = 40;
+let valordoultra = 60;
+let tresa4 = 5;
+let maisque4 = 7.50;
+
+let tipodrassinatura = prompt("digite o nome da assinatura qur voce desej;")
+let quantitelas = promot ("digite quantas telas voce terá")
+if (tipodeassinatura == "básico"){
+    if (quantitelas >=3 && quantitels<=4){
+        alert ("O seu plano será R$"+(valordobasico+maisque4))
+    }else if (quantitelas>=3> && quantitelas<=4){
+        alert ("o seus lano será R$" +(valordobasico+maisque4))
+    }else {
+        alert ("o seu plano será R$"+valordobasico)
+    }
+}
+else if (tipodeassinatura == "premium"){
+    if (quantitelas >=3 && quantitels<=4){
+        alert ("O seu plano será R$"+(valordopremium+maisque4))
+    }else if (quantitelas>=3> && quantitelas<=4){
+        alert ("o seus lano será R$" +(valordopremium+maisque4))
+    }else {
+        alert ("o seu plano será R$"+valordopremium)
+    }
+}
+else (tipodeassinatura == "ultra"){
+    if (quantitelas >=3 && quantitels<=4){
+        alert ("O seu plano será R$"+(valordoultra+maisque4))
+    }else if (quantitelas>=3> && quantitelas<=4){
+        alert ("o seus lano será R$" +(valordoultra+maisque4))
+    }else {
+        alert ("o seu plano será R$"+valordoultra)
+    }
+}
+let daysOfweek = ["Raquel", "Maraisa",80,40,10];  
+console.log (soysOfwee[1])
+console.log (daysOfweek.length)
+let nomedoaluno = prompt("digite o nome do novo aluno")
+daysOfweek.unshift(nomedoaluno)
+daysOfweek.unshift("Miguel", "Allyson", "Arthur", "Nayline")
+daysOfweek.push("JOÃO")
+daysOfweek.pop()
+daysOfweek.shift()
+daysOfweek.sort()
+daysOfweek.reverse()
+alert ("atualmente os alunos da escola são" +daysOfweek)
+alert(daysOfweek.indexOf("Arthur"))*/
